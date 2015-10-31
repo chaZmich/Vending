9d10bb148f089b04d525a50a94134635b6bd78e8
chazmich <apoda@ruswizards.com>
2015-10-31 14:31:38 +0300

    Initial code upload
    Just basic core of the app to store as base

---
+++ UnitTests/VendingMachineTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachine.Vending;
+using Moq;
+using VendingDevice = VendingMachine.Vending.VendingMachine;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class VendingMachineTests
+    {
+        [TestMethod]
+        public void BasicTest()
+        {
+            var mock = new Mock<IVendingMachine>();
+
+            mock.Setup(framework => framework.ReturnMoney())
+                .Returns(new Money());
+            
+            Assert.AreEqual(mock.Object.ReturnMoney(), new Money());
+        }
+
+        [TestMethod]
+        public void test1()
+        {
+            var mock = new Mock<VendingDevice>();
+            Assert.IsFalse(mock.Object.Manufacturer == String.Empty);
+        }
+    }
+}
---
+++ VendingMachine/Vending/VendingMachine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Vending
+{
+    /// <summary>
+    /// Vending machine basic implementation
+    /// </summary>
+    public class VendingMachine : IVendingMachine
+    {
+        public VendingMachine(string manufacturer)
+        {
+
+        }
+
+        /// <summary>
+        /// Do not allow creating vending machines without manufacturer set
+        /// So hiding default constructor
+        /// </summary>
+        private VendingMachine()
+        {
+
+        }
+
+        public string Manufacturer
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Money Amount
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Product[] Products
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Money InsertCoin(Money amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Money ReturnMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Buy(int productNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
